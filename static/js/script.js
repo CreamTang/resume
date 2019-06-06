93bbaba64098a754ca4e0b7df831784d6e779d57
CreamTang <51407242+CreamTang@users.noreply.github.com>
2019-06-06 20:47:44 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
+$(document).ready(function($){
+    $("*").removeAttr('contenteditable');        
+})
 
-
-  // First, checks if it isn't implemented yet.
+<!   // First, checks if it isn't implemented yet.
 if (!String.prototype.format) {
   String.prototype.format = function() {
     var args = arguments;
@@ -90,3 +92,5 @@ jQuery(document).ready(function($) {
     // $("*").removeAttr('contenteditable');
 
 });     
+
+> 
